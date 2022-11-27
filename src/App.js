2d01e7f485b40fe393c29a6ba1a5546030cedf79
shourovahmd <moh.abuhanifa@gmail.com>
2022-11-28 02:32:43 +0600

    nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>
-  
+    <div className="mx-40 my-10">
+      <nav className="flex justify-between text-[#323231] font-semibold">
+        <div>Abu Hanifa</div>
+        <div className="flex justify-between gap-10">
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Sills</a>
+          <a href="/">Service</a>
+          <a href="/">Portfolio</a>
+          <a href="/">Contact</a>
+        </div>
+      </nav>
     </div>
   );
 }
