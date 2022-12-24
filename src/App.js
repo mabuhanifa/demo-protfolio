f314b8f7b4f429b9080ada6d8cecc762d131bd4f
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-24 17:23:09 +0600

    main

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
-import { FiGithub, FiInstagram } from "react-icons/fi";
-import { RiFacebookCircleLine } from "react-icons/ri";
-import { TbSend } from "react-icons/tb";
+
 import "./App.css";
+import Main from "./components/Main";
 import Nav from "./components/Nav";
 function App() {
   return (
     <div className="2xl:mx-40 xl:mx-20 my-10">
       <Nav/>
-      <section className="flex my-40">
-        <div className="flex items-center">
-          {/* -----icons----- */}
-          <div className="flex flex-col gap-10 text-2xl mr-20">
-            <span>
-              <FiInstagram />
-            </span>
-            <span>
-              <FiGithub />
-            </span>
-            <span>
-              <RiFacebookCircleLine />
-            </span>
-          </div>
-        </div>
-        <section className="mx-5 xl:mx-20 flex ">
-          <div className="w-full my-20">
-            <h1 className="text-4xl font-bold">Mohammed Abu Hanifa</h1>
-            <p className="text-2xl font-semibold my-2 dash">Web Developer</p>
-            <p className="text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Temporibus illo incidunt autem quia optio soluta atque corrupti!
-              Provident accusamus id omnis harum nesciunt blanditiis facilis.
-            </p>
-            <button className="px-10 py-5 rounded-2xl bg-gray-700 text-gray-100 my-5 ">
-              Say Hello
-              <TbSend className="inline mx-2 text-2xl" />
-            </button>
-          </div>
-          <div className="w-full flex justify-center items-center mx-10">
-            <img
-              src="https://avatars.githubusercontent.com/u/90899296?v=4"
-              alt=""
-              className="w-96 rounded-xl"
-            />
-          </div>
-        </section>
-      </section>
+      <Main/>
+      
     </div>
   );
 }
