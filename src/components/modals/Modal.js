a1b8beaf3639629f7eef4d563413d828d089e884
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-25 00:58:29 +0600

    modal functionality updated

--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -1,13 +1,27 @@
 import React from "react";
 
-export default function Modal({ view,setModal }) {
+export default function Modal({ view, setModal }) {
   if (!view) return null;
+  const closeModal = (e) => {
+    if (e.target.id === "container") {
+      setModal((m) => !m);
+    }
+  };
   return (
-    <div className="bg-blue-300 bg-opacity-30 backdrop-blur-sm fixed inset-0 flex justify-center items-center z-15">
+    <div
+      id="container"
+      className="bg-blue-300 bg-opacity-30 backdrop-blur-sm fixed inset-0 flex justify-center items-center z-15"
+      onClick={closeModal}
+    >
       <div className="border-[2px] border-blue-300 rounded-xl p-10">
         <h1>Modal</h1>
 
-        <button className="bg-red-500 p-2 rounded text-white" onClick={()=>setModal()}>Close</button>
+        <button
+          className="bg-red-500 p-2 rounded text-white"
+          onClick={() => setModal((m) => !m)}
+        >
+          Close
+        </button>
       </div>
     </div>
   );
