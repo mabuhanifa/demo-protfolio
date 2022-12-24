b86d7c9e66fefa06fe312b25eb217686acdb9105
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-25 00:11:01 +0600

    Modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import "./App.css";
+import About from "./components/About";
 import Main from "./components/Main";
 import Nav from "./components/Nav";
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="2xl:mx-40 xl:mx-20 my-10">
       <Nav/>
       <Main/>
+      <About/>
       
     </div>
   );
